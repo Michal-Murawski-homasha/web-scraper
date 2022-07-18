87c5dcd911b860389a7ac7d444c0865f88404560
Michał Murawski <homasha@gmail.com>
2022-07-18 20:14:17 +0200

    Adding headings h2.

--- index.php
+++ index.php
@@ -10,12 +10,12 @@
   <?php
 
     $web = file_get_contents('https://pl.wikipedia.org/wiki/Miasta_w_Polsce#Lista_miast_w_Polsce_w_porz%C4%85dku_alfabetycznym');
-    echo 'Ilość znaków przed przetworzeniem: '.strlen($web).'<br>';
-    echo $web;
+    echo '<h2>Ilość znaków przed przetworzeniem: '.strlen($web). '</h2><br>';
+    echo $web.'<br>';
     $textStripTags = strip_tags($web, ['p','li','br']);
     // $textStripTags = str_replace('/\<div\>\<\/div\>/', '/\<br\>/', $web);
-    echo 'Ilość znaków po przetworzeniu '.strlen($textStripTags).'<br>';
-    echo "<p>$textStripTags</p>";
+    echo '<h2>Ilość znaków po przetworzeniu '.strlen($textStripTags). '</h2><br>';
+    echo "<p>$textStripTags</p><br>";
     // $textStripTagsDiv = strip_tags($web, ['div']);
     // $textStripTagsLi = strip_tags($web, ['li']);
     // $textStripTagsA = strip_tags($web, ['a']);
